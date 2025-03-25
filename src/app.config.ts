492dcfdc0666bab451329b4a8e8221987ebf5875
swlws <979623074@qq.com>
2025-03-25 11:10:57 +0800

    添加tabBar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,13 +1,31 @@
 export default defineAppConfig({
-  pages: [
-    'pages/home/index',
-    'pages/task/index',
-    'pages/mine/index',
-  ],
+  pages: ["pages/home/index", "pages/task/index", "pages/mine/index"],
+  tabBar: {
+    list: [
+      {
+        pagePath: "pages/home/index",
+        text: "",
+        iconPath: "assets/icon_menu.png",
+        selectedIconPath: "assets/icon_menu--checked.png",
+      },
+      {
+        pagePath: "pages/task/index",
+        text: "",
+        iconPath: "assets/icon_menu.png",
+        selectedIconPath: "assets/icon_menu--checked.png",
+      },
+      {
+        pagePath: "pages/mine/index",
+        text: "",
+        iconPath: "assets/icon_menu.png",
+        selectedIconPath: "assets/icon_menu--checked.png",
+      },
+    ],
+  },
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});
