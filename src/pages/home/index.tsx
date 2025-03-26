a313eb364c17996fe6c3cf5e790f585b388a9b10
swlws <979623074@qq.com>
2025-03-26 09:45:37 +0800

    引入taro-ui

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,12 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
+// 若项目配置了别名 @ 指向 src 目录，确保路径正确
+// import NForm from '@/components/form/index';
+// 若未配置别名，使用相对路径
+import NForm from '../../components/n-form/index';
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
   })
@@ -10,6 +14,10 @@ export default function Index () {
   return (
     <View className='index'>
       <Text>Home</Text>
+      <NForm onSubmit={() =>{}}>
+        {/* 表单内容 */}
+      </NForm>
     </View>
+
   )
 }
