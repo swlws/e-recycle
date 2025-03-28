294e328a70f345c9f2e567fe08cc3aeab46265b0
swlws <979623074@qq.com>
2025-03-28 11:16:59 +0800

    跳转子页面

--- src/taro-api/router.ts
+++ src/taro-api/router.ts
@@ -1,8 +1,7 @@
-import { ROUTE_PATH } from "@/constants/route";
+import { ENUM_ROUTE_PATH } from "@/constants/route";
 import Taro from "@tarojs/taro";
 
 // 使用 Taro 的 API 跳转路由
-export function gotoPage(path: ROUTE_PATH) {
+export function gotoPage(path: ENUM_ROUTE_PATH) {
   Taro.navigateTo({ url: path });
-
 }
