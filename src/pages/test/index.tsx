84821c512875d2537e0ef0099d691a977854eb52
swlws <979623074@qq.com>
2025-03-28 11:58:38 +0800

    Test page

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,13 +1,13 @@
 import { View } from "@tarojs/components";
-
 import ChildPageLayout from "@/layout/child-page-layout";
+import LoginForm from "./components/login-form";
 
 export default function ChildPage() {
   return (
     <ChildPageLayout>
       <View>测试页面</View>
 
-
+      <LoginForm />
     </ChildPageLayout>
   );
 }
