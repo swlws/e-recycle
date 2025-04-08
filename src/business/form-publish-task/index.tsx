45575c3329b1d52567f3ae98374ad4b8756d5c80
swlws <979623074@qq.com>
2025-04-08 19:35:11 +0800

    config

--- src/business/form-publish-task/index.tsx
+++ src/business/form-publish-task/index.tsx
@@ -1,8 +1,8 @@
-import { Button, Form } from "@taroify/core";
-import { View } from "@tarojs/components";
-import { useRef, useState } from "react";
-import TaskName from "./components/task-name";
-import PhoneNumber from "./components/phone-number";
+import { Button, Form } from '@taroify/core';
+import { View } from '@tarojs/components';
+import { useRef, useState } from 'react';
+import TaskName from './components/task-name';
+import PhoneNumber from './components/phone-number';
 
 // Define the type for the Form instance
 interface FormInstance {
@@ -20,18 +20,24 @@ export default function BeFormPublishTask(props: BeFormPublishTaskProps) {
   const [formValue, setFormValue] = useState(props.formValue || {});
 
   const handleChange = (key: string, value: any) => {
-    setFormValue({ ...formValue, [key]: value });
+    setFormValue({
+      ...formValue,
+      [key]: value,
+    });
 
     if (props.onChange) {
-      props.onChange({ ...formValue, [key]: value });
+      props.onChange({
+        ...formValue,
+        [key]: value,
+      });
     }
   };
 
   const submit = () => {
-    console.log("提交表单", formValue, formRef.current);
+    console.log('提交表单', formValue, formRef.current);
     formRef.current?.validate().then((r) => {
-      console.log("表单验证结果", r);
-      console.log("表单值", formRef.current?.getValues());
+      console.log('表单验证结果', r);
+      console.log('表单值', formRef.current?.getValues());
     });
   };
 
@@ -43,13 +49,7 @@ export default function BeFormPublishTask(props: BeFormPublishTaskProps) {
         <PhoneNumber value={formValue.phoneNumber} onChange={handleChange}></PhoneNumber>
       </Form>
 
-      <Button
-        shape='round'
-        block
-        color='primary'
-        formType='submit'
-        onClick={submit}
-      >
+      <Button shape="round" block color="primary" formType="submit" onClick={submit}>
         提交
       </Button>
     </View>
