de60ce11894c8ace404d17673471cd3a6990928f
swlws <979623074@qq.com>
2025-04-11 22:27:40 +0800

    trst

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,26 +1,10 @@
 import ChildPageLayout from '@/layout/child-page-layout';
-import BeFormPublishTask from '@/business/form-publish-task';
-import { ITaskInfo } from '@/typings/task';
-
-const getDefaultFormValue = (): ITaskInfo => {
-  return {
-    person: '',
-    phoneNumber: '',
-    address: '',
-    goods: [],
-  };
-};
+import { View } from '@tarojs/components';
 
 export default function ChildPage() {
-  let formValue: ITaskInfo = getDefaultFormValue();
-
-  const handleFormValueChange = (newFormValue: ITaskInfo) => {
-    formValue = newFormValue;
-  };
-
   return (
     <ChildPageLayout>
-      <BeFormPublishTask formValue={formValue} />
+      <View>Test</View>
     </ChildPageLayout>
   );
 }
