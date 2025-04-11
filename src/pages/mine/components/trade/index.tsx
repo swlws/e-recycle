1670772b8119e26ac27512fb421ec724e75504c7
swlws <979623074@qq.com>
2025-04-11 09:43:53 +0800

    交易

---
+++ src/pages/mine/components/trade/index.tsx
@@ -0,0 +1,25 @@
+import { Badge, Cell, Grid } from '@taroify/core';
+import { PhotoOutlined } from '@taroify/icons';
+import { View } from '@tarojs/components';
+
+export default function Trade() {
+  return (
+    <View className="page-trade">
+      <Cell title="交易"></Cell>
+
+      <Grid>
+        <Grid.Item
+          icon={<PhotoOutlined />}
+          badge={<Badge content={10} max={99} />}
+          text="我发布的"
+        />
+
+        <Grid.Item
+          icon={<PhotoOutlined />}
+          badge={<Badge content={100} max={99} />}
+          text="我卖出的"
+        />
+      </Grid>
+    </View>
+  );
+}
--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,10 +1,16 @@
 import { View } from '@tarojs/components';
 import UserInfo from './components/user-info';
+import Trade from './components/trade';
+import { Cell } from '@taroify/core';
 
 export default function Mine() {
   return (
     <View className="page-mine">
       <UserInfo></UserInfo>
+
+      <Cell.Group inset style={{ marginTop: '16px' }}>
+        <Trade></Trade>
+      </Cell.Group>
     </View>
   );
 }
