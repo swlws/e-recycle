e2eb156a7b4e9658fd4709914c565dec9738ea21
swlws <979623074@qq.com>
2025-04-16 15:38:32 +0800

    任务中心

--- src/pages/task-center/index.tsx
+++ src/pages/task-center/index.tsx
@@ -1,9 +1,25 @@
+import { Search } from '@taroify/core';
 import { View } from '@tarojs/components';
+
+import './index.scss';
+import { useState } from 'react';
 import PullAndLoadMoreList from '@/business/pull-and-load-more-list';
 
 export default function TaskCenter() {
+  const [searchValue, setSearchValue] = useState('');
+
+  function onSearch(value: string) {
+    setSearchValue(value);
+  }
+
   return (
-    <View>
+    <View className="task-center">
+      <Search
+        value={searchValue}
+        placeholder="请输入搜索关键词"
+        onChange={(e) => onSearch(e.detail.value)}
+      />
+
       <PullAndLoadMoreList />
     </View>
   );
