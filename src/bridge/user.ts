26535e0854d4b093a81c10210512b8779db49786
swlws <979623074@qq.com>
2025-04-18 10:38:57 +0800

    user

--- src/bridge/user.ts
+++ src/bridge/user.ts
@@ -14,7 +14,7 @@ export function getUserProfile(): Promise<UserProfile> {
     Taro.getUserProfile({
       desc: '用于完善用户昵称、头像', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        console.log(res.userInfo);
+        console.log('getUserProfile', res);
         resolve(res.userInfo);
       },
       fail: (err) => {
