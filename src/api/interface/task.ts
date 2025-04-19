fb4ca6355b6f5756422669ebced9359a139cc1fd
swlws <979623074@qq.com>
2025-04-19 23:14:46 +0800

    用户自己发布过的任务

--- src/api/interface/task.ts
+++ src/api/interface/task.ts
@@ -1,11 +1,19 @@
 export default {
+  /** 新建任务 */
   createTask: {
     url: '/api/task/create',
     method: 'POST',
     header: {},
   },
+  /** 查询所有任务 */
   queryAllTask: {
     url: '/api/task/all',
+    method: 'POST',
+    header: {},
+  },
+  /** 查询自己发布过的任务 */
+  querySelfPublishedTask: {
+    url: '/api/task/self/published',
     method: 'POST',
     header: {},
   },
