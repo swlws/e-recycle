29b52212755ca5317bc4d9e3178304582ddd18cc
swlws <979623074@qq.com>
2025-04-20 13:01:54 +0800

    任务状态

--- src/constants/public.ts
+++ src/constants/public.ts
@@ -2,3 +2,12 @@
  * 商品分类
  */
 export const GOODS_LIST = ['杂物', '家具', '家电', '书籍', '建筑材料', '拆迁杂物'];
+
+/**
+ * 任务状态
+ */
+export enum ENUM_TASK_STATE {
+  PENDDING = 'PENDDING',
+  WILL_RESOLVE = 'WILL_RESOLVE', // 即将执行
+  RESOLVE = 'RESOLVE',
+}
