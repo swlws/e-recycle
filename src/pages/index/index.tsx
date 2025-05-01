f260fcdc18df4275880127ff61f55b863ba37447
swlws <979623074@qq.com>
2025-05-01 12:23:47 +0800

    邀请

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,27 @@
 import { useLoad } from '@tarojs/taro';
 import MainPageLayout from '@/layout/main-page-layout';
 import { MainPageList } from './data';
+import CacheMgr from '@/cache';
 
 import api from '@/api';
 
 import './index.scss';
+import { getCurrentInstance } from '@tarojs/runtime';
+
+function checkNetwork() {
+  api.ops.alive().then(() => {
+    console.log('network alive');
+  });
+}
 
 export default function Index() {
   useLoad(() => {
-    api.ops.alive().then(() => {
-      console.log('network alive');
-    });
+    // 检查网络
+    checkNetwork();
+
+    // 邀请人ID
+    const inviteUid = getCurrentInstance().router?.params?.scene as string;
+    CacheMgr.inviter.setValue(inviteUid);
   });
 
   return <MainPageLayout mainPageList={MainPageList} />;
