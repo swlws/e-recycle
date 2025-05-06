093a03bf6c45e34b5f9218cc2f3750cc87e600f9
swlws <979623074@qq.com>
2025-05-06 08:48:15 +0800

    yanghi

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -22,13 +22,12 @@ export default function Mine() {
     <View className="page-mine">
       <UserInfo onLoginSuccess={handleLoginSuccess}></UserInfo>
 
-      <Cell.Group inset style={{ marginTop: '16px' }}>
+      <Cell.Group inset style={{ marginTop: '24px' }}>
         <Trade ref={tradeRef}></Trade>
       </Cell.Group>
 
-      <Cell.Group inset style={{ marginTop: '16px' }}>
+      <Cell.Group inset style={{ marginTop: '24px' }}>
         <QrCode></QrCode>
-
         <UserScore></UserScore>
       </Cell.Group>
     </View>
