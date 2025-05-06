0fece324de35796c4dd90b0b1db3200864425384
swlws <979623074@qq.com>
2025-05-06 08:26:17 +0800

    share

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,11 @@
-import { useLoad } from '@tarojs/taro';
+import { useLoad, getCurrentInstance, useShareAppMessage, useShareTimeline } from '@tarojs/taro';
 import MainPageLayout from '@/layout/main-page-layout';
 import { MainPageList } from './data';
 import CacheMgr from '@/cache';
 import { addRecond, ENUM_LOG_TYPE } from '@/monitor';
 import api from '@/api';
+import LogoImg from '@/asset/logo.png';
+import JiuWuHuiShou from '@/asset/images/jiuwuhuishou.png';
 
 import './index.scss';
 
@@ -14,6 +16,26 @@ function checkNetwork() {
 }
 
 export default function Index() {
+  // 分享给朋友
+  useShareAppMessage(() => {
+    const uid = CacheMgr.user.value._id;
+    return {
+      title: 'E速收 - 让回收变得更快更高效',
+      path: `/pages/index/index?scene=${uid}&orgin=friend`,
+      imageUrl: JiuWuHuiShou, // 5:4
+    };
+  });
+
+  // 分享到朋友圈
+  useShareTimeline(() => {
+    const uid = CacheMgr.user.value._id;
+    return {
+      title: 'E速收 - 让回收变得更快更高效',
+      query: `scene=${uid}&orgin=timeline`,
+      imageUrl: LogoImg, // 1:1
+    };
+  });
+
   useLoad((options) => {
     // 检查网络
     checkNetwork();
